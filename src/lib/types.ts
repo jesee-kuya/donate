140bae0ced7425edb57812c819054b6eceacd3e8
jkuya <kuyajesee@proton.me>
2025-06-11 04:34:46 +0300

    feat: add project export

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,2 +1,8 @@
 // src/lib/types.ts
 export type NavLink = { href: string; label: string };
+export type Project = {
+    id: string;
+    title: string;
+    description?: string;
+    totalRaised?: number;
+  };
