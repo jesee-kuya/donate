cf0eb6ff3feee6569ab07e0e9a068c9e92b2949a
jkuya <kuyajesee@proton.me>
2025-06-11 04:21:11 +0300

    feat: add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    const year = new Date().getFullYear();
+  </script>
+  
+  <footer class="mt-10 p-4 border-t text-sm text-center text-var(--text-muted-color)">
+    Built with ❤️ for a better world. | 
+    <a href="/admin" class="underline hover:text-var(--primary-hover-color)">Admin</a> | © {year}
+  </footer>
+  
