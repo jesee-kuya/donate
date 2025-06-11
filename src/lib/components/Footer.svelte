a3e68b3b8cdd8e2e800509c46d6fc5dedcba0716
jkuya <kuyajesee@proton.me>
2025-06-11 03:34:41 +0300

    add Footer component with a message and admin link

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,4 @@
+<footer class="mt-10 p-4 border-t text-sm text-center text-gray-500">
+    Built with ❤️ for a better world. | <a href="/admin" class="underline text-blue-600">Admin</a>
+  </footer>
+  
