62c19cc47300337bedd9d03898bbb79016a57bdf
jkuya <kuyajesee@proton.me>
2025-06-11 04:47:02 +0300

    feat: add type Donation

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,14 @@
 // src/lib/types.ts
 export type NavLink = { href: string; label: string };
 export type Project = {
-    id: string;
-    title: string;
-    description?: string;
-    totalRaised?: number;
-  };
+  id: string;
+  title: string;
+  description?: string;
+  totalRaised?: number;
+};
+
+export type Donation = {
+  project: string;
+  amount: number;
+  date: string; // ISO string
+};
